40253fd489d811c709b801c7043f1247f8559b05
Sabrina Bilmak <bilmak.sabrina@gmail.com>
2025-04-27 16:14:15 +0200

    add response model

--- fast_api.py
+++ fast_api.py
@@ -2,26 +2,36 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
+class Item(BaseModel):
+    text: str | None = None
+    is_done: bool = False
+
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-    return "Gello"
+    return "Hello"
+
 
 items = []
+
+
 @app.post("/items")
-def create_item(item:str):
+def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get("/items/{item_id}")
-def get_item(item_id:int):
-    if item_id<len(items):
+
+@app.get("/items", response_model=list[Item])
+def get_list_items(limit: int = 10):
+    return items[0:limit]
+
+
+@app.get("/items/{item_id}", response_model=list[Item])
+def get_item(item_id: int) -> Item:
+    if item_id < len(items):
         return items[item_id]
     else:
         raise HTTPException(status_code=404, detail="Item not found")
-    
-@app.get("/items")
-def get_items_limit(limit:int=10):
-    return items[0:limit]
