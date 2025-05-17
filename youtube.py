e12e175ba41613559703c8421c94208eba4e9ea1
Sabrina Bilmak <bilmak.sabrina@gmail.com>
2025-05-17 19:32:56 +0200

    youtube downloader

---
+++ youtube.py
@@ -0,0 +1,18 @@
+import yt_dlp #yt_dlp is a modern alternative to youtube-dl
+import sys
+
+if len(sys.argv) < 2:
+    print("Input your URL")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+ydl_opts = {
+    'format': 'best',
+    'outtmpl': '%(title)s.%(ext)s' #specifies a filename pattern
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(url, download=False) 
+    print(f"Title: {info['title']}")
+    ydl.download([url])
