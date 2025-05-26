a5921bd49d91ae4a28f3bde8e5c43ec62886c833
Sabrina Bilmak <bilmak.sabrina@gmail.com>
2025-05-26 16:30:43 +0200

    test voice project

---
+++ voice_generation.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+from pydub import AudioSegment
+
+
+
+
+def creating_mp3(text):
+    tts= gTTS(text, lang='en')
+    tts.save("test_audio.mp3")
+    
+    sound = AudioSegment.from_mp3("test_audio.mp3")
+    sound.export("voice.wav", format="wav")
+ 
+text = "Hello, this is a test version for your project."   
+creating_mp3(text)
